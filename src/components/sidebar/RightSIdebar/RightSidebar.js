a882fea1cef0cf69ae99d9ff27d57653d15391e1
[udamadu11] <madu12dara@gmail.com>
2021-07-05 16:35:42 +0530

    left sidebar chart

--- src/components/sidebar/RightSIdebar/RightSidebar.js
+++ src/components/sidebar/RightSIdebar/RightSidebar.js
@@ -2,12 +2,27 @@ import React from "react";
 import "./RightSidebar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-
+import { ChartNames } from "../../../Data/ChartNames";
+import CardComponent from "../sidebarComponent/CardComponent";
 const RightSidebar = ({ isOpenSidebar, toggle2 }) => {
   return (
     <div className={isOpenSidebar ? "rightSidebar" : "is-closed"}>
       <div className="sidebar-toggle">
         <FontAwesomeIcon icon={faTimes} className="icon" onClick={toggle2} />
+      </div>
+      <div className="sidebar_container">
+        <div className="search_input">
+          <input type="text" placeholder="Search chart here" />
+        </div>
+        <div className="chart_details">
+          {ChartNames.map((data, i) => {
+            return (
+              <div>
+                <CardComponent type={data.type} img={data.img} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
---
+++ src/components/sidebar/sidebarComponent/CardComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Card, CardContent, CardHeader, Typography } from "@material-ui/core";
+import "./CardComponent.css";
+const CardComponent = ({ type, img }) => {
+  console.log(type);
+  return (
+    <div>
+      <Card className="chart_card" style={{ width: 200, height: 160 }}>
+        <Typography
+          className="chart_title"
+          variant="subtitle1"
+          style={{ marginLeft: 20 }}
+        >
+          {type}
+        </Typography>
+        <CardContent>
+          <img src={`img/${img}`} style={{ height: 100, width: 150 }} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default CardComponent;
